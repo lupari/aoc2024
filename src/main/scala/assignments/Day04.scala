b0eb13415bb5a1bd3f907842a93ad126cbbd1fa8
Matti Lupari <matti.lupari@gmail.com>
2024-12-04 22:24:32 +0200

    Update Day04.scala

--- src/main/scala/assignments/Day04.scala
+++ src/main/scala/assignments/Day04.scala
@@ -19,7 +19,7 @@ object Day04:
     val d2 = Seq(p.downleft(), p, p.upright())
     val d3 = Seq(p.upleft(), p, p.downright())
     val d4 = Seq(p.downright(), p, p.upleft())
-    Seq(d1, d2, d3, d4).map(_.map(grid(_)).mkString).count(_ == "MAS") == 2
+    Seq(d1, d1.reverse, d3, d3.reverse).map(_.map(grid(_)).mkString).count(_ == "MAS") == 2
 
   def partOne(): Int = grid.keys.toList.flatMap(words).count(_ == "XMAS")
   def partTwo(): Int = grid.keys.count(isXmas)
