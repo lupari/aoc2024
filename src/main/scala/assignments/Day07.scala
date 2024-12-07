626060d7e3673cb8b1f7d0e0791becff0360160d
Matti Lupari <matti.lupari@gmail.com>
2024-12-07 14:29:56 +0200

    Update Day07.scala

--- src/main/scala/assignments/Day07.scala
+++ src/main/scala/assignments/Day07.scala
@@ -4,28 +4,25 @@ import scala.io.Source
 
 object Day07:
 
-  case class Equation(testValue: Long, operands: List[Int]):
-    def results(concat: Boolean = false): Int =
-      def helper(xs: List[Int], expr: List[String], value: Long): List[List[String]] = xs match
-        case Nil => if value == testValue then List(expr) else Nil
+  case class Equation(test: Long, operands: List[Int]):
+    def passes(concat: Boolean = false): Boolean =
+      def helper(xs: List[Int], expr: List[String], acc: Long): List[List[String]] = xs match
+        case Nil => if acc == test then List(expr) else Nil
         case h :: t =>
-          val sums     = helper(t, expr :+ "+" :+ s"$h", value + h)
-          val products = helper(t, expr :+ "*" :+ s"$h", value * h)
-          val concats = if concat then helper(t, expr :+ "||" :+ s"$h", s"$value$h".toLong) else Nil
+          val sums     = helper(t, expr :+ "+" :+ s"$h", acc + h)
+          val products = helper(t, expr :+ "*" :+ s"$h", acc * h)
+          val concats  = if concat then helper(t, expr :+ "||" :+ s"$h", s"$acc$h".toLong) else Nil
           sums ++ products ++ concats
 
       operands match
-        case h :: t => helper(t, List(s"$h"), h).length
-        case _      => 0
+        case h :: t => helper(t, List(s"$h"), h).nonEmpty
+        case _      => false
 
   def parse(s: String): Equation =
     val Seq(r, ops) = s.split(": ").toSeq
     Equation(r.toLong, ops.split("\\s+").map(_.toInt).toList)
 
-  val input: Seq[Equation] = Source.fromResource("day07.txt").getLines.toList.map(parse)
+  val input: List[Equation] = Source.fromResource("day07.txt").getLines.toList.map(parse)
 
-  def partOne(): Long =
-    input.map(e => (e.testValue, e.results())).filter(_._2 > 0).map(_._1).sum
-
-  def partTwo(): Long =
-    input.map(e => (e.testValue, e.results(true))).filter(_._2 > 0).map(_._1).sum
+  def partOne(): Long = input.filter(_.passes()).map(_.test).sum
+  def partTwo(): Long = input.filter(_.passes(true)).map(_.test).sum
