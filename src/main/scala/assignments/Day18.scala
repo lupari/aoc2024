0188bbd1ed725f3713a93c754990fc8700ec2279
Matti Lupari <matti.lupari@gmail.com>
2024-12-18 09:03:10 +0200

    day 18

--- src/main/scala/assignments/Day18.scala
+++ src/main/scala/assignments/Day18.scala
@@ -1,40 +1,22 @@
 package assignments
 
+import scala.annotation.tailrec
 import scala.io.Source
 import lib.Points.Point
 import lib.Graphs
-import scala.annotation.tailrec
+import lib.Search
 
 object Day18:
 
-  def parse(l: String) = l match
+  def parse(l: String): Point = l match
     case s"$x,$y" => Point(x.toInt, y.toInt)
-  val bytes = Source.fromResource("day18.txt").getLines.map(parse).toList
-  val start = Point(0,0)
-  val end = Point(70,70)
+  val bytes: List[Point]           = Source.fromResource("day18.txt").getLines.map(parse).toList
+  val (start, end): (Point, Point) = (Point(0, 0), Point(70, 70))
 
-  def withinBounds(p: Point) = p.x >= 0 && p.y >= 0 && p.x < end.x+1 && p.y < end.y+1
-  def search(nf: Point => Set[Point]) = Graphs.bfs.search(start)(nf)(_ == end)
+  def isOpen(p: Point, bytes: Seq[Point]): Boolean = p <=> end && !bytes.contains(p)
+  def search()(nf: Point => Set[Point]): Option[Int] = Graphs.bfs.search(start)(nf)(_ == end)
 
-  def findFirstBlock(xs: List[Point]): Point = 
-    @tailrec
-    def helper(lower: Int, upper: Int): Point =
-      if lower >= upper then xs(lower-1)
-      else
-        val mid = (lower + upper) / 2
-        val xs1 = xs.take(mid)
-        def nf(p: Point) = p.neighbors.filter(n => withinBounds(n) && !xs1.contains(n)).toSet
-        search(nf) match
-          case None => helper(lower, mid - 1)
-          case Some(_) => helper(mid + 1, upper)
-
-    helper(1, xs.length + 1)
-
-  def partOne(): Int = 
-    val kiloByte = bytes.take(1024)
-    def nf(p: Point) = p.neighbors.filter(n => withinBounds(n) && !kiloByte.contains(n)).toSet
-    search(nf).get
-
+  def partOne(): Int = search()(_.neighbors.filter(isOpen(_, bytes.take(1024))).toSet).get
   def partTwo(): String = 
-    val point = findFirstBlock(bytes)
-    s"${point.x},${point.y}"
+    def hasPath(xs: Iterable[Point]) = search()(_.neighbors.filter(isOpen(_, xs.toSeq)).toSet)
+    Search.binSearch(bytes)(hasPath)._1.last.toString
--- src/main/scala/lib/Graphs.scala
+++ src/main/scala/lib/Graphs.scala
@@ -4,7 +4,6 @@ import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 import scala.collection.mutable
 import scala.collection.mutable.PriorityQueue
-import lib.Points.Position.neighbors
 
 object Graphs:
   object dfs:
